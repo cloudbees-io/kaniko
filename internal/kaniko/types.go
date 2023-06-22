0129c67d7d0711e5e98d602db33ed693c9372871
Pinaki Sarkar <psarkar@cloudbees.com>
2023-06-22 15:51:54 -0700

    feat: [SDP-3696] Removed unused types

--- internal/kaniko/types.go
+++ internal/kaniko/types.go
@@ -6,18 +6,12 @@ type Config struct {
 	context.Context
 	// ExecutablePath is the path to the Kaniko executor binary.
 	ExecutablePath string
-	// Dockerconfigjson contains the registry and registry credentials
-	// DockerConfigJson string `json:"dockerconfigjson,omitempty"`
 	// Dockerfile is the path to the Dockerfile to build.
 	Dockerfile string `json:"dockerfile,omitempty"`
 	// Context is the path to the build context.
 	DockerContext string `json:"context,omitempty"`
 	// Destination is the destination of the built image.
 	Destination string `json:"destination,omitempty"`
-	// BuildArgs are the args passed to the Docker build.
-	// BuildArgs string `json:"build-args,omitempty"`
-	// Labels are the labels passed to the Docker build.
-	// Labels   string `json:"labels,omitempty"`
 }
 
 type DockerConfigJson struct {
