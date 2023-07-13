9c96db6029ea5d98223ee6735606203894baf320
Max Goltzsche <mgoltzsche@cloudbees.com>
2023-07-13 23:07:25 +0200

    feat: SDP-6160: provide image digest as output

--- internal/kaniko/execute_test.go
+++ internal/kaniko/execute_test.go
@@ -99,6 +99,7 @@ func Test_cmdBuilder(t *testing.T) {
 	}
 	os.Setenv("DOCKER_BUILD_ARGS", "key1=value1,key2=value2")
 	os.Setenv("DOCKER_LABELS", "key_l1=l_value1,key_l2=l_value2")
+	os.Setenv("CLOUDBEES_OUTPUTS", "/tmp/fake-outputs")
 
 	cmd, err := c.cmdBuilder()
 	require.NoError(t, err)
@@ -120,6 +121,8 @@ func Test_cmdBuilder(t *testing.T) {
 		"key_l1=l_value1",
 		"--label",
 		"key_l2=l_value2",
+		"--digest-file",
+		"/tmp/fake-outputs/digest",
 	}
 	expectedCmd := exec.CommandContext(ctx, "/kaniko/executor", exepectedArgs...)
 
