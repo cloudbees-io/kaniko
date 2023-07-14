314c4406fd146256e7425f98e7a535aecc9d2aac
Max Goltzsche <mgoltzsche@cloudbees.com>
2023-07-14 18:53:23 +0200

    feat: SDP-6210: add outputs tag, tag-digest, image

--- internal/kaniko/execute.go
+++ internal/kaniko/execute.go
@@ -19,14 +19,63 @@ func (k *Config) Run(ctx context.Context) (err error) {
 
 	k.lookupBinary()
 
-	kanikoCmd, err := k.cmdBuilder()
+	outDir := os.Getenv("CLOUDBEES_OUTPUTS")
+	digestFile := ""
+	if outDir != "" {
+		digestFile = filepath.Join(os.TempDir(), "kaniko-image-digest")
+	}
+
+	kanikoCmd, err := k.cmdBuilder(digestFile)
 	if err != nil {
 		return fmt.Errorf("failed to build kaniko command: %w", err)
 	}
 
 	fmt.Printf("Running command: %s\n", kanikoCmd.String())
 
-	return kanikoCmd.Run()
+	err = kanikoCmd.Run()
+	if err != nil {
+		return fmt.Errorf("run kaniko: %w", err)
+	}
+
+	if outDir != "" {
+		err = k.writeActionOutputs(outDir, digestFile)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (k *Config) writeActionOutputs(outDir, digestFile string) error {
+	dest := k.processDestinations()[0]
+	tag := "latest"
+	if pos := strings.Index(dest, ":"); pos > 0 && pos < len(dest)-1 {
+		tag = dest[pos+1:]
+		dest = dest[:pos]
+	}
+	digest, err := os.ReadFile(digestFile)
+	if err != nil {
+		return fmt.Errorf("read kaniko image digest: %w", err)
+	}
+	err = os.WriteFile(filepath.Join(outDir, "digest"), digest, 0640)
+	if err != nil {
+		return fmt.Errorf("write digest output: %w", err)
+	}
+	err = os.WriteFile(filepath.Join(outDir, "tag"), []byte(tag), 0640)
+	if err != nil {
+		return fmt.Errorf("write tag output: %w", err)
+	}
+	tagDigest := fmt.Sprintf("%s@%s", tag, string(digest))
+	err = os.WriteFile(filepath.Join(outDir, "tag-digest"), []byte(tagDigest), 0640)
+	if err != nil {
+		return fmt.Errorf("write tag-digest output: %w", err)
+	}
+	imageRef := fmt.Sprintf("%s:%s@%s", dest, tag, string(digest))
+	err = os.WriteFile(filepath.Join(outDir, "image"), []byte(imageRef), 0640)
+	if err != nil {
+		return fmt.Errorf("write image output: %w", err)
+	}
+	return nil
 }
 
 func (k *Config) processDestinations() []string {
@@ -65,7 +114,7 @@ func (k *Config) env() []string {
 	return os.Environ()
 }
 
-func (k *Config) cmdBuilder() (*exec.Cmd, error) {
+func (k *Config) cmdBuilder(digestFile string) (*exec.Cmd, error) {
 	cmdArgs := []string{
 		"--verbosity=debug",
 		"--ignore-path=/cloudbees/",
@@ -91,8 +140,8 @@ func (k *Config) cmdBuilder() (*exec.Cmd, error) {
 		cmdArgs = append(cmdArgs, "--label", label)
 	}
 
-	if outDir := os.Getenv("CLOUDBEES_OUTPUTS"); outDir != "" {
-		cmdArgs = append(cmdArgs, "--digest-file", filepath.Join(outDir, "digest"))
+	if digestFile != "" {
+		cmdArgs = append(cmdArgs, "--digest-file", digestFile)
 	}
 
 	kanikoCmd := exec.CommandContext(k.Context, k.ExecutablePath, cmdArgs...)
